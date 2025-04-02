67cd9d0c51ee35d92b8605d986b9dcb3a29f04d3
rouxru <stefanroux69@gmail.com>
2025-04-02 19:49:10 +0000

    feat: Player can now move forward and backward chore: Use groups instead of invoking methods directly on objects

--- main.py
+++ main.py
@@ -7,17 +7,21 @@ from player import Player
 def main():
     init_text()
     pygame.init()
-    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updateables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    Player.containers = (updateables, drawables)
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt=dt)
-        player.draw(screen=screen)
+        updateables.update(dt=dt)
+        for drawable in drawables:
+            drawable.draw(screen=screen)
         pygame.display.flip()
         dt = clock.tick() / 1000
 
--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_SPEED, PLAYER_TURN_SPEED
 import pygame
 
 
@@ -29,11 +29,22 @@ class Player(CircleShape):
         """TODO"""
         self.rotation += PLAYER_TURN_SPEED * dt
 
-    def update(self, dt) -> None:
+    def update(self, dt: int) -> None:
         """TODO"""
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
             self.rotate(dt=dt * -1)
+
         if keys[pygame.K_d]:
             self.rotate(dt=dt)
+
+        if keys[pygame.K_w]:
+            self.move(dt=dt)
+
+        if keys[pygame.K_s]:
+            self.move(dt=dt * -1)
+
+    def move(self, dt: int):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
