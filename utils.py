778be17789a43ef4ac6b1ec8dcf014d742143fa6
rouxru <stefanroux69@gmail.com>
2025-04-02 17:46:27 +0000

    feat: Initial game loop with blank window

---
+++ utils.py
@@ -0,0 +1,8 @@
+from constants import SCREEN_WIDTH
+from constants import SCREEN_HEIGHT
+
+
+def init_text():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
